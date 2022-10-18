ea139a0de7b386c981d9b2e43612716c83617220
TG229 <114865111+TG229@users.noreply.github.com>
2022-10-18 11:54:54 +0200

    altered version of exercise 1 containing foreign key binds

---
+++ alter1.sql
@@ -0,0 +1,41 @@
+CREATE schema segreteria;
+
+CREATE DOMAIN segreteria.votazione AS SMALLINT CHECK(value >= 18 and value <= 30 or value = 33);
+
+CREATE TABLE segreteria.studenti (
+	matricola CHAR(5) PRIMARY KEY,
+	nome VARCHAR(20) NOT NULL,
+	cognome VARCHAR(20) NOT NULL,
+	data_nascita DATE,
+	anno_corso SMALLINT DEFAULT 1 CHECK(anno_corso > 0 and anno_corso < 4) ,
+		UNIQUE (nome, cognome, data_nascita)
+);
+
+CREATE TABLE segreteria.docenti(
+	cod_docente CHAR(5) PRIMARY KEY,
+	nome VARCHAR(20) NOT NULL,
+	cognome VARCHAR(20) NOT NULL,
+	indirizzo VARCHAR(50)
+);
+
+CREATE TABLE segreteria.corsi(
+	cod_corso CHAR(3) PRIMARY KEY,
+	nome VARCHAR(20),
+	docente CHAR(5),
+		FOREIGN KEY (docente) REFERENCES segreteria.docenti (cod_docente)
+		ON DELETE RESTRICT
+		ON UPDATE CASCADE
+);
+
+CREATE TABLE segreteria.esami(
+	studente CHAR(5),
+	corso CHAR(3),
+	data DATE NOT NULL,
+	voto segreteria.votazione NOT NULL,
+		FOREIGN KEY (studente) REFERENCES segreteria.studenti (matricola)
+		ON DELETE RESTRICT
+		ON UPDATE CASCADE,
+		FOREIGN KEY (corso) REFERENCES segreteria.corsi (cod_corso)
+		ON DELETE RESTRICT
+		ON UPDATE CASCADE
+);
